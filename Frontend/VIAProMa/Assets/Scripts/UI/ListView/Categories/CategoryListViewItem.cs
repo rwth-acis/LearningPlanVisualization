47f26dc2b45cc650ea7c302f79bddb6436a85886
Yul Eberlein <yul.eberlein@rwth-aachen.de>
2020-10-01 12:59:08 +0200

    Renamed Project

--- Frontend/VIAProMa/Assets/Scripts/UI/ListView/Categories/CategoryListViewItem.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(CategoryDataDisplay))]
-public class CategoryListViewItem : ListViewItem<Category>
-{
-}
